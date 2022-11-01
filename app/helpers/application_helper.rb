6a3b3540ca0fbe5ee6bb5d7767037ad901a9a89e
Takahiro920615 <deaman6948@gmail.com>
2022-11-01 13:12:28 +0000

    titleカスタムヘルパー設定完了

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  
+  #カスタムヘルパー　page_name ="" で指定文字をビューに入力することで反映できるようになる。
+  def full_title(page_name ="")
+    base_title = "AttendanceApp"
+    if page_name.empty?
+      base_title
+    else
+      page_name + "|" + base_title
+    end
+  end
 end
