dcae3dc5af51fa935a32e8c88ff55df2c8626c2c
Takahiro920615 <deaman6948@gmail.com>
2022-11-02 14:03:21 +0000

    User モデル完成

---
+++ db/migrate/20221102135655_create_users.rb
@@ -0,0 +1,18 @@
+class CreateUsers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :password
+      t.string :password_digest
+      t.datetime :basic_time
+      t.datetime :designated_work_start_time
+      t.datetime :designated_work_end_time
+      t.boolean :superior
+      t.boolean :superior
+      t.boolean :admin
+
+      t.timestamps
+    end
+  end
+end
