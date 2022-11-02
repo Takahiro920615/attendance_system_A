302a50d3b98362d28ca709c68c502ba6c55f4908
Takahiro920615 <deaman6948@gmail.com>
2022-11-02 13:24:22 +0000

    Userモデル作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   root 'attendance_page#top'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
