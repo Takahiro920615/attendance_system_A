b6d8da3fc1a4cb60392eb7a4fd652b1442bcca62
Takahiro920615 <deaman6948@gmail.com>
2022-11-06 14:07:31 +0000

    usermodel エラーメッセージ実装完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,32 @@ class UsersController < ApplicationController
     @user = User.new
   end
   
+ def create
+   @user = User.new(user_params)
+   if @user.save
+      
+   else
+     flash[:danger] = "ユーザー登録に失敗しました"
+     render :new
+   end
+ end
+ 
+ def index
+ end
+ 
+ 
+  
   def show
     @user = User.find(params[:id])
   end
 end
+
+
+
+
+private
+
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
