ae20bd9a4ec527ee11776515127c4119f92fec91
Takahiro920615 <deaman6948@gmail.com>
2022-11-07 14:35:33 +0000

    ログイン機能実装完了

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   get '/signup', to:'users#new'
 
   root 'attendance_page#top'
+  
+  get '/login', to:'sessions#new'
+  post '/login', to:'sessions#create'
+  delete 'logout', to:'sessions#destroy'
 
  resources :users
 end
