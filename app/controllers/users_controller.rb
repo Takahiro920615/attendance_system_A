29e12a9290057f61d045ddaf40c3578bc6d678ce
Takahiro920615 <deaman6948@gmail.com>
2022-11-09 04:28:21 +0000

    ユーザー編集ページ作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,6 +24,9 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+def edit
+  @user = User.find(params[:id])
 end
 
 
@@ -35,3 +38,4 @@ private
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
+end
