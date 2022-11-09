d664ba68869269a46930eca586dcd098aabe7e6f
Takahiro920615 <deaman6948@gmail.com>
2022-11-09 05:57:23 +0000

    beforeアクションでアクセス制限作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :set_user, only:[:show, :edit, :update]
+  before_action :logged_in_user, only: [:show,:edit,:update] #管理者のみに変更する(admin)
+  before_action :correct_user, only: [:edit,:update] #管理者のみに変更する(admin)
   def new
     @user = User.new
   end
@@ -21,15 +24,12 @@ class UsersController < ApplicationController
  
  
   def show
-    @user = User.find(params[:id])
   end
 
   def edit
-    @user = User.find(params[:id])
   end
   
   def update
-    @user = User.find(params[:id])
       if @user.update_attributes(user_params)
         flash[:success] = "ユーザー情報を更新しました。"
         redirect_to @user
@@ -41,10 +41,26 @@ class UsersController < ApplicationController
 
 
 
-private
+ private
 
 
-  def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  end
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+    
+    def set_user
+      @user = User.find(params[:id])
+    end
+    
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "ログインしてください。"
+        redirect_to login_url
+      end
+    end
+    
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to (root_url) unless @user == current_user
+    end
 end
