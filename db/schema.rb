da13f83e63f2971d70ba399bc9268c55d3c2db80
Takahiro920615 <deaman6948@gmail.com>
2022-11-15 14:05:58 +0000

    出退勤ボタン作成

--- db/schema.rb
+++ db/schema.rb
@@ -36,8 +36,8 @@ ActiveRecord::Schema.define(version: 20221114121447) do
     t.datetime "updated_at", null: false
     t.string "remember_digest"
     t.string "department"
-    t.datetime "basic_time", default: "2022-11-13 23:00:00"
-    t.datetime "work_time", default: "2022-11-13 22:30:00"
+    t.datetime "basic_time", default: "2022-11-14 23:00:00"
+    t.datetime "work_time", default: "2022-11-14 22:30:00"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
