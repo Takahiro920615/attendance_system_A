5d4d0777329acbd69acb9d5b43d5055a9f5b11a4
Takahiro920615 <deaman6948@gmail.com>
2022-11-17 14:13:19 +0000

    １ヶ月まとめて更新実装完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -68,26 +68,6 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password, :password_confirmation,:department)
     end
     
-    def set_user
-      @user = User.find(params[:id])
-    end
-    
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "ログインしてください。"
-        redirect_to login_url
-      end
-    end
-    
-    def correct_user
-      @user = User.find(params[:id])
-      redirect_to (root_url) unless @user == current_user
-    end
-    
-    def admin_user
-      redirect_to root_url unless current_user.admin?
-    end
     
     def basic_info_params
       params.require(:user).permit(:department, :basic_time, :work_time)
--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -5,8 +5,15 @@ class Attendance < ApplicationRecord
   validates :note,length: { maximum: 50}
 
   validate :started_at_need_finished_at
+  validate :started_at_fast_than_finished_at
 
   def started_at_need_finished_at
     errors.add(:started_at, "が必要です") if started_at.blank? && finished_at.present?
+  end
+  
+  def started_at_fast_than_finished_at
+    if started_at.present? && finished_at.present?
+      errors.add(:started_at, "より早い退勤時間は無効です") if started_at > finished_at
+    end
   end
 end
