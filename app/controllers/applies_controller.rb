08ec64b3049220e5138fe6e7180454e8702f54d9
Takahiro920615 <deaman6948@gmail.com>
2022-12-05 08:21:13 +0000

    apply申請システム途中１２０５１７２１

--- app/controllers/applies_controller.rb
+++ app/controllers/applies_controller.rb
@@ -1,15 +1,26 @@
 class AppliesController < ApplicationController
   before_action :select_superiors
   
+  def request_one_month
+    
+    
+  end
+
+  
   def receve_application_content
   @receve_application_content = Apply.order(:one_month).where(application_to_superior: @user.id, application_content: "申請中").group_by(&:user_id)
   end
+
+  def apply_application_content
+  end
+
+
 end
 
 private
  
  
  
- 
+
   
  
--- app/models/apply.rb
+++ app/models/apply.rb
@@ -1,3 +1,5 @@
 class Apply < ApplicationRecord
   belongs_to :user, optional: true
+  
+  enum one_month_request_status: { なし: 0, 申請中: 1, 承認: 2, 否認: 3 }, _prefix: true
 end
