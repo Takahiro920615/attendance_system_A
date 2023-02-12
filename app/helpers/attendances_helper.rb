b547380580c5ec82d03804f84ed58cfa46a0ef79
Takahiro920615 <deaman6948@gmail.com>
2023-02-12 14:14:01 +0000

    残業申請承認モーダルページ９０％完成

--- app/helpers/attendances_helper.rb
+++ app/helpers/attendances_helper.rb
@@ -12,4 +12,14 @@ module AttendancesHelper
     def working_times(start, finish)
       format("%.2f",(((finish - start)/60)/60))
     end
+    
+    #残業時間のフォーマット
+    def overtime_culculation(change_end_time,designated_work_end_time,overtime_next_day )
+      if overtime_next_day == "1"
+        format("%.2f",((change_end_time-designated_work_end_time.hour)+ (change_end_time.min+designated_work_end_time.min)/60.0)+24)
+      else overtime_next_day == "0"
+        format("%.2f", (change_end_time.hour - designated_work_end_time.hour) + (change_end_time.min - designated_work_end_time.min) / 60.0)
+      end
+    end
+        
 end
