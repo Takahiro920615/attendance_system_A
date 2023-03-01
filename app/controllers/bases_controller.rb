36bef867c0acd75e8f35a3db64ab2f75d19fe110
Takahiro920615 <deaman6948@gmail.com>
2023-03-01 08:10:40 +0000

    baseのeditページ作り直し

--- app/controllers/bases_controller.rb
+++ app/controllers/bases_controller.rb
@@ -1,40 +1,41 @@
 class BasesController < ApplicationController
  before_action :admin_user, only: [:index]
-   
+ before_action :set_base, only: [:index,:edit]
+ 
+  def index
+    @bases = Base.all
+    @base = Base.new
+  end 
   
-  def new
-   @base = Base.new
-  end
   
   def create
-    @base = Base.create(base_params)
+    @base = Base.new(base_params)
      if @base.save
-       flash[:success] = "拠点情報の作成に成功しました。"
+       flash[:success] = "拠点情報を追加しました"
        redirect_to bases_url
      else
-       render :new
+      flash[:danger] = "拠点情報を追加できません"
+      @base = Base.all
+      render :index
      end
   end
   
-  def index
-    @bases = Base.all.order(id: "DESC")
-  end
+  
    
   def show
   end
   
   def edit
-   @base = Base.find(params[:id])
   end
   
   def update
    @base = Base.find(params[:id])
-     if @base.update_attributes(base_params)
-       flash[:succsss] = "拠点情報を更新しました。"
+    if @base.update_attributes(base_params)
+    flash[:success] = "拠点情報を更新しました"
     redirect_to bases_url
-     else
+    else
      render :edit
-     end
+    end
   end
     
   def destroy
@@ -49,4 +50,9 @@ class BasesController < ApplicationController
    def base_params
      params.require(:base).permit(:base_no, :base_name, :attendance_type )
    end
+   
+   def set_base
+      @base = Base.find_by(params[:id])
+   end
+    
 end
