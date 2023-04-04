4cdd9d157add14f9fd44f130d20f68f65ec6f8b8
EC2 Default User <ec2-user@ip-172-31-21-43.ap-northeast-1.compute.internal>
2023-04-04 21:36:21 +0900

    修正０４０４２３要確認

--- app/helpers/attendances_helper.rb
+++ app/helpers/attendances_helper.rb
@@ -11,7 +11,7 @@ module AttendancesHelper
     
     def working_times(started_at, finished_at, change)
       if finished_at.present? && started_at.present?
-        if change == "1"
+        if change == true
           format("%.2f",(((finished_at.hour - started_at.hour))/60.0/60.0)+24)
         else
           format("%.2f",((finished_at - started_at)/60.0)/60.0)
